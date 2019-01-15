0217a6d2d9e6c35d6b5ae29715e688f926b9806c
shubhamAsolkar22 <38282971+shubhamAsolkar22@users.noreply.github.com>
2019-01-15 21:38:23 +0530

    forgotten lang attr on html

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = `picture of ${restaurant.name}`; 
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
